3956dd15cd11ec52fadd313d50f7b3804c9f2bd7
Vinh Giang <vinhgiang@Vinhs-MacBook-Pro.local>
2023-12-14 15:06:25 -0700

    display category

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:meals/models/category.dart';
+
+import '../data/dummy_data.dart';
+import '../widgets/category_grid_item.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
@@ -11,18 +14,16 @@ class CategoryScreen extends StatelessWidget {
         title: const Text('Pick your category!'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('1'),
-          Text('2'),
-          Text('3'),
-          Text('4'),
-          Text('5'),
+        children: [
+          for (final Category categor in availableCategories)
+            CategoryGridItem(category: categor),
         ],
       ),
     );
