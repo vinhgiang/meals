d237810a2095ce1e022e6a70a22ac6576c764200
Vinh Giang <giangcamvinh@gmail.com>
2024-02-14 00:53:17 -0700

    use different icon for farovite meal

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -13,6 +13,10 @@ class MealDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -31,8 +35,8 @@ class MealDetailScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(
-              Icons.star,
+            icon: Icon(
+              isFavorite ? Icons.star : Icons.star_border,
             ),
           ),
         ],
